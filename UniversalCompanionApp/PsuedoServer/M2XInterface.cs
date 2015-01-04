cca7017d8c504c01a826e3bfd21844eb24327ce1
unknown <codewarriormalo@gmail.com>
2015-01-03 22:05:03 -0700

    adding suer

--- UniversalCompanionApp/PsuedoServer/M2XInterface.cs
+++ UniversalCompanionApp/PsuedoServer/M2XInterface.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using ATTM2X;
+using System.Data.SqlClient;
 
 using System.Reflection;
 namespace PsuedoServer
@@ -28,11 +29,65 @@ namespace PsuedoServer
                 string a2 = string.Format("status: {0}", _M2XClient.LastResponse.Json.status);
                 string a3 = string.Format("description: {0}", _M2XClient.LastResponse.Json.description);
 
-                _AccelXStream = _M2XDevice.Stream("accel_x");
-                _AccelYStream = _M2XDevice.Stream("accel_y");
-                _AccelZStream = _M2XDevice.Stream("accel_z");
- //               _LatStream = _M2XDevice.Stream("lat");
-//                _LngStream = _M2XDevice.Stream("lng");
+
+                DateTime now = DateTime.Now;
+                string dateFormatter = "yyyy-MM-ddTHH:mm:ss.ffffffZ";
+                var m2xFilter = new { start = now.AddMinutes(-5).ToString(dateFormatter), end = now.ToString(dateFormatter), limit = 100 };
+                
+                _AccelXStream = _M2XDevice.Stream("accel-x");
+                var x_vals = _AccelXStream.Values(m2xFilter);
+                var jx_vals = x_vals.Json.values;
+                _AccelYStream = _M2XDevice.Stream("accel-y");
+                var y_vals = _AccelYStream.Values(m2xFilter);
+                var jy_vals = y_vals.Json.values;
+                _AccelZStream = _M2XDevice.Stream("accel-z");
+                var z_vals = _AccelZStream.Values(m2xFilter);
+                var jz_vals = z_vals.Json.values;
+
+                SqlConnectionStringBuilder csBuilder = new SqlConnectionStringBuilder();
+                //csBuilder.DataSource = "i2ym1ip5n8.database.windows.net,1433";
+                csBuilder.DataSource = "wrj8boouxc.database.windows.net,1433";
+                csBuilder.InitialCatalog = "WachterData";
+                csBuilder.UserID = "Hackathon";
+                csBuilder.Password = "Hacking2104";
+                SqlConnection conn = new SqlConnection(csBuilder.ConnectionString);
+                conn.Open();
+
+                // with lat and lng
+                //string dataFormatter = "INSERT INTO [dbo].[RawData] ([Id], [AccelX], [AccelY], [AccelZ], [GeoLat], [GeoLng], [DataTime]) VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6})";
+
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT Max(ID) from RawData";
+                int maxId = 0;
+                try
+                {
+                    maxId = (int)cmd.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+
+                }
+                int new_id = maxId + 1;
+
+                // without lat and lng
+                string dataFormatter = "INSERT INTO [dbo].[RawData] ([AccelX], [AccelY], [AccelZ], [GeoLat], [GeoLng], [TimeStamp]) VALUES ({0}, {1}, {2}, {3}, {4}, '{5}')";
+                string insertStatement = string.Empty;
+                float x; float y; float z; float lat; float lng;
+                for (int i = 0; i < jx_vals.Count; i++)
+                {
+                    new_id += i;
+                    x = (float)jx_vals[i].value;
+                    y = (float)jy_vals[i].value;
+                    z = (float)jz_vals[i].value;
+                    lat = 0;
+                    lng = 0;
+                
+                    string timestamp = jx_vals[i].timestamp;
+                    insertStatement = string.Format(dataFormatter, x, y, z, lat, lng, timestamp);
+                    cmd.CommandText = insertStatement;
+                    cmd.ExecuteNonQuery();
+                }
+
             }
             catch(Exception ex)
             {
--- UniversalCompanionApp/PsuedoServer/Program.cs
+++ UniversalCompanionApp/PsuedoServer/Program.cs
@@ -10,7 +10,8 @@ namespace PsuedoServer
         static void Main(string[] args)
         {
             M2XInterface m = new M2XInterface();
-            m.InitializeM2X("26244b19a6376e900e4ca0fe14803f3e", "f63302dddf5a42e63bffe192e6fcb920");
+            m.InitializeM2X("26244b19a6376e900e4ca0fe14803f3e", "df5e75e6785ac96a66dcc30087f56e73");
+            //df5e75e6785ac96a66dcc30087f56e73
         }
     }
 }
